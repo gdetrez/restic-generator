45a71ea60407cd0936ecd536cd76cd42b0a55a40
Grégoire Détrez <gregoire@mullvad.net>
2022-06-30 17:43:51 +0200

    Generate separate forget and prune services

--- tests/output.rs
+++ tests/output.rs
@@ -9,13 +9,19 @@ macro_rules! snapshot_test {
             let normal_dir = TempDir::new()?;
             let early_dir = TempDir::new()?;
             let late_dir = TempDir::new()?;
-            let mut cmd = Command::cargo_bin("systemd-restic-generator")?;
+            let mut cmd = Command::cargo_bin("restic-generator")?;
             cmd.arg("-c")
                 .arg($config)
                 .arg(normal_dir.path())
                 .arg(early_dir.path())
                 .arg(late_dir.path());
             cmd.assert().success();
+
+            let paths = std::fs::read_dir(normal_dir.path()).unwrap();
+            for path in paths {
+                println!("Name: {}", path.unwrap().path().display())
+            }
+
             insta::assert_snapshot!(read_to_string(normal_dir.path().join($output))?);
             Ok(())
         }
@@ -26,6 +32,18 @@ snapshot_test!(
     local_backup_service,
     "example-config.toml",
     "restic-myrepo-backup.service"
+);
+
+snapshot_test!(
+    local_forget_service,
+    "example-config.toml",
+    "restic-myrepo-forget.service"
+);
+
+snapshot_test!(
+    local_prune_service,
+    "example-config.toml",
+    "restic-myrepo-prune.service"
 );
 
 snapshot_test!(
